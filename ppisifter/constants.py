bf18f2ea5dddd34c06c7946b0a1df3e33934aae4
Haiyang Hou <66674907+houhaiyang@users.noreply.github.com>
2026-06-04 14:40:38 +0800

    Delete ppisifter directory

--- ppisifter/constants.py
+++
@@ -1,9 +0,0 @@
-"""Project-wide constants."""
-
-DEFAULT_SEED = 42
-EPS = 1e-8
-POSITIVE_LABEL = 1
-NEGATIVE_LABEL = 0
-DEFAULT_EMBEDDING_SUFFIX = ".residue_emb.npy"
-DEFAULT_SPLITS = ("train", "valid", "test")
-SUPPORTED_SPLIT_STRATEGIES = ("random", "protein_disjoint")
